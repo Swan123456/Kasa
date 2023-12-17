ad6265f5eb7fe9ba2e034fc34ced748ec8745ab4
Swan <swan.marin@epitech.eu>
2023-12-17 13:29:02 +0100

    Home page

--- kasa/src/components/Footer/Footer.jsx
+++ kasa/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className="foot-container">
 			<img src={logo} alt="logo de kasa" />
-			<p className="foot-container__text">© 2020 Kasa. All rights reserved</p>
+			<p className="foot-container__text">© 2023 Kasa. All rights reserved</p>
 		</footer>
 	);
 }
