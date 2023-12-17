8fbea1d12c0cc605cc3ced5647d98a113ee46f8c
Swan <swan.marin@epitech.eu>
2023-12-17 14:13:00 +0100

    About page

--- kasa/src/pages/About/About.jsx
+++ kasa/src/pages/About/About.jsx
@@ -1,7 +1,19 @@
+import AboutBanner from "../../components/AboutBanner/AboutBanner";
+import Collapse from "../../components/Collapse/Collapse";
+import aboutArray from "../../datas/aboutArray.json"; 
+
 export default function About() {
 	return (
 		<>
-
+			<AboutBanner />
+			{aboutArray.map((rule, id) => (
+				<Collapse
+					key={id}
+					aboutTitle={rule.aboutTitle}
+					aboutText={rule.aboutText}
+					aboutStyle="about-style"
+				/>
+			))}
 		</>
 	);
 }
