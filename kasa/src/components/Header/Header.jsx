15d17f746246216b0acdb3f113d0c827eb8915b8
Swan <swan.marin@epitech.eu>
2023-12-17 19:15:58 +0100

    Modification

--- kasa/src/components/Header/Header.jsx
+++ kasa/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "../../assets/images/LOGO.svg";
 import Nav from "../Nav/Nav";
+import { Link } from "react-router-dom";
 
 export default function Header() {
 	return (
 		<header className="headerWrap">
 			<figure className="headerWrap__fig">
-				<img className="logo" src={logo} alt="logo de l'agence kasa" />
+				<Link to="/">
+					<img className="logo" src={logo} alt="logo de l'agence kasa" />
+				</Link>
 			</figure>
 			<Nav className="nav-header" />
 		</header>
--- kasa/src/pages/Logement/Logement.jsx
+++ kasa/src/pages/Logement/Logement.jsx
@@ -3,24 +3,46 @@ import { useParams, useNavigate } from "react-router-dom";
 import Carrousel from "../../components/Slideshow/Slideshow";
 import Collapse from "../../components/Collapse/Collapse";
 import axios from "axios";
+import Host from "../../components/Host/Host";
+import Rate from "../../components/Rate/Rate";
+import Tag from "../../components/Tag/Tag";
+import Loader from "../../components/Loader/Loader";
 
 export default function Logement() {
 	const params = useParams();
 	const navigate = useNavigate();
 
 	const [pickedAppart, setPickedAppart] = useState();
-	useEffect(() => {
-		const getData = async () => {
-			const res = await axios.get("/logements.json"); //j'ai préféré utiliser une requète AXIOS pour être prêt à la future mise en place de l'API
-			const picked = res.data.find(({ id }) => id === params.id);
-			res.data.map(() => setPickedAppart(picked));
-			if (picked === undefined) {
-				navigate("/404", { state: { message: "Can't get data" } }); //renvoi vers la page 404 en cas d'URL de logement invalide
-			}
-		};
-		getData();
-	}, []); // array vide du useEffect pour ne lancer qu'une seule fois
+	const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+				await new Promise(resolve => setTimeout(resolve, 500));
+
+                const res = await axios.get("/logements.json");
+                const picked = res.data.find(({ id }) => id === params.id);
+                setPickedAppart(picked);
+                setLoading(false);
+                if (picked === undefined) {
+                    navigate("/404", { state: { message: "Can't get data" } });
+                }
+            } catch (error) {
+                console.error("Erreur lors du chargement des données :", error);
+                setLoading(false);
+                navigate("/404", { state: { message: "Error loading data" } });
+            }
+        };
+		
+        getData();
+    }, [params.id, navigate]);
+	
+	if (loading) {
+		return <Loader />;
+	}
+
 	const slidePics = pickedAppart && pickedAppart.pictures;
+	const tags = pickedAppart && pickedAppart.tags;
 	const equipments = pickedAppart && pickedAppart.equipments;
 	const equip =
 		pickedAppart &&
@@ -34,7 +56,30 @@ export default function Logement() {
 			<div key={params.id} className="fiche-container">
 				<Carrousel slides={slidePics} />
 				<section className="hostInfo-container">
-
+				<section className="hostInfo-container">
+					<div className="title-tags-container">
+						<div className="title-container redFont">
+							<h1>{pickedAppart.title}</h1>
+							<h3>{pickedAppart.location}</h3>
+						</div>
+						<div className="tags-container">
+							{tags.map((tag) => (
+								<Tag key={tag} tag={tag} />
+							))}
+						</div>
+					</div>
+					<div className="rate-host-container">
+						<div className="host-container redFont">
+							<Host
+								hostName={pickedAppart.host.name}
+								hostPic={pickedAppart.host.picture}
+							/>
+						</div>
+						<div className="rate-container">
+							<Rate score={pickedAppart.rating} />
+						</div>
+					</div>
+				</section>
 				</section>
 				<div className="collapse-fiche-container">
 					<Collapse
